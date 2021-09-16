4ebc8d1af537ec30d17708cf90751ad6c1c610c3
ahkohd <victor.olorunbunmi@gmail.com>
2021-09-16 14:24:57 +0100

    chore: write test

--- src/utils/__test__/fns.test.ts
+++ src/utils/__test__/fns.test.ts
@@ -3,5 +3,6 @@ import { formatNumber } from "../fns";
 describe("utility functions", () => {
   it("should format number", () => {
     expect(formatNumber(1000)).toEqual("1.0k");
+    expect(formatNumber(4300)).toEqual("4.3k");
   });
 });
